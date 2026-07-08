2143a218434965898bd9f714dc6a3b9c6739512f
kourosh <afsharkouroshpv@gmail.com>
2026-07-08 23:23:32 +0330

    feat: Implement main screen with custom app bar and theme support

---
+++ lib/core/components/custom_app_bar.dart
@@ -0,0 +1,26 @@
+import 'package:curriculum_vitae/core/components/utils/constants.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Size get preferredSize => Size.fromHeight(60);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text('data'),
+      titleSpacing: 20,
+      actionsPadding: EdgeInsets.only(right: 20),
+      backgroundColor: AppColors.darkAppBar,
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.light_mode),
+          splashRadius: 20,
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/feature/main/presentation/screen/main_screen.dart
@@ -0,0 +1,19 @@
+import 'package:curriculum_vitae/core/components/custom_app_bar.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(),
+      body: Center(child: Text('data')),
+    );
+  }
+}
